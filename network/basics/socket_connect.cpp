8b4b870acb62b59f9356b28920a0d63db44e6803
Lucas Menke <lucas_menke@icloud.com>
2024-03-03 10:17:34 +0100

    > tcp connection

--- network/basics/socket_connect.cpp
+++ network/basics/socket_connect.cpp
@@ -1,11 +1,8 @@
-#include <stdio.h>
 #include <string>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
 
 int main()
 {
